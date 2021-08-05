0584e4d344ed87e196ca2ba005c7bbfbb6b096be
ellis882 <ellisrachel1304@me.com>
2021-08-05 10:55:19 +0000

    made block comments shorter

--- hangman.py
+++ hangman.py
@@ -6,8 +6,7 @@ def get_word():
     secret_word = random.choice(word_list)
     return secret_word.upper()
 
-def play(secret_word):
-    # variables that will be updated during the play of Hangman when player takes turns to guess the secret word
+def play(secret_word):    
     reveal = "_" * len(secret_word)
     gameWon = False
     guessed_letters = []
@@ -31,30 +30,33 @@ def play(secret_word):
     print("\n")
     print(f"You have {lives} lives")
     print("\n")
-    # code in while loop runs either until secret word is guessed or player runs out of lives
+    # runs either until secret word is guessed or player lost lives
     while gameWon == False and lives > 0:
         guess = input("Guess a letter or word: ").upper()
-        # if letter in alphabet is already guessed, or is not in the secret word, you lose one live
+        # if letter alphabet already is guessed
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print(f"You already guessed the letter {guess}")
+            # letter not in secret word, lose one live    
             elif guess not in secret_word:
                 print(f"{guess} is not in the secret word.")
                 lives -= 1
                 guessed_letters.append(guess)
             else:
-                # use list comprehension to see if guess is in secret word, and reveal all occurences of guess. If guessed all letters you win
+                # list comprehension to see if guess is in secret word
                 print(f"Good job, {guess} is in the secret word!")
                 guessed_letters.append(guess)
                 word_as_list = list(reveal)
                 indices = [i for i, letter in enumerate(secret_word) if letter == guess]
+                # reveal all occurences of guess
                 for index in indices:
                     word_as_list[index] = guess
                 reveal = "".join(word_as_list)
+                # if guessed all letters you win
                 if "_" not in reveal:
                     gameWon = True    
 
-        # if player already guessed a specific word that is not the secret word, or input is not the secret word, you lose one live.                   
+        # statements for if word guess is wrong                   
         elif len(guess) == len(secret_word) and guess.isalpha():
             if guess in guessed_words:
                 print(f"You already guessed that word, {guess}")
@@ -68,7 +70,8 @@ def play(secret_word):
                 reveal = secret_word    
 
         else:
-            # if input is not a letter from alphabet or word that is not the same length of secret word
+            # input is not a letter from alphabet
+            # input is not same length as secret word
             print("Not a valid guess. Please try again!")
         print(display_hangman(lives))
         print(f"SECRET WORD: {reveal}")
@@ -82,7 +85,7 @@ def play(secret_word):
 
 
 def display_hangman(lives):
-    # index of each stage that corresponts with the number of lives that the player have
+    # index stage that corresponts with the number of lives player have
 
     stages = [  # final state: head, torso, both arms, and both legs
                 """
@@ -170,5 +173,5 @@ def main():
 # code frame so that program will run script on command line
 if __name__ == "__main__":    
     main()
-print("-------------------------------------------------- \n")
+
 
